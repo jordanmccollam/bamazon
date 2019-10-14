1dfc9bbd314c1decc41075032c1c20372184a2f2
Jordan McCollam <jordy.mccollam@gmail.com>
2019-10-14 16:44:04 -0500

    Set up inquirer with prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,19 @@
 var inquirer = require("inquirer");
+
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What's the ID of the item you are searching for?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "How many units would you like to buy?",
+        name: "units"
+    }
+]).then(answers => {
+
+    console.log("\nID: " + answers.id);
+    console.log("Units: " + answers.units);
+
+})
