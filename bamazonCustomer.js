56e97d07109482faa87551417671a0e2aca2a64f
Jordan McCollam <jordy.mccollam@gmail.com>
2019-10-15 12:30:37 -0500

    Displayed products on run

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,20 +1,50 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-inquirer.prompt([
-    {
-        type: "input",
-        message: "What's the ID of the item you are searching for?",
-        name: "id"
-    },
-    {
-        type: "input",
-        message: "How many units would you like to buy?",
-        name: "units"
-    }
-]).then(answers => {
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "jojo1997",
+    database: "bamazon"
+});
 
-    console.log("\nID: " + answers.id);
-    console.log("Units: " + answers.units);
-
+connection.connect(function (err) {
+    if (err) throw err;
+    displayProducts();
 })
+
+function displayProducts() {
+    connection.query(
+        "SELECT * FROM products",
+        function (err, res) {
+            if (err) throw err;
+
+            console.log("\nPRODUCTS *********************************");
+            for (var i = 0; i < res.length; i++) {
+                console.log("\n" + res[i].item_id  + ". " + res[i].product_name);
+                console.log("$" + res[i].price + "\n");
+            }
+            console.log("******************************************");
+        }
+    )
+}
+
+function prompt() {
+    inquirer.prompt([{
+            type: "input",
+            message: "What's the ID of the item you would like to buy?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "How many units would you like to buy?",
+            name: "units"
+        }
+    ]).then(answers => {
+
+        console.log("\nID: " + answers.id);
+        console.log("Units: " + answers.units);
+
+    })
+}
