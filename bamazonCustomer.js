e80c2910d56b74ffe7ee7bce4e119c342f0e645c
Jordan McCollam <jordy.mccollam@gmail.com>
2019-10-15 13:47:41 -0500

    Complete BamazonCustomer App V1

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,7 +22,7 @@ function displayProducts() {
 
             console.log("\nPRODUCTS *********************************");
             for (var i = 0; i < res.length; i++) {
-                console.log("\n" + res[i].item_id  + ". " + res[i].product_name + " || $" + res[i].price);
+                console.log("\n" + res[i].item_id + ". " + res[i].product_name + " || $" + res[i].price);
             }
             console.log("\n******************************************");
             promptCustomer();
@@ -42,9 +42,45 @@ function promptCustomer() {
             name: "units"
         }
     ]).then(answers => {
+        // console.log("\nID: " + answers.id);
+        // console.log("Units: " + answers.units);
 
-        console.log("\nID: " + answers.id);
-        console.log("Units: " + answers.units);
+        // If quantity of desired item > 0 . . . 
+        connection.query(
+            "SELECT * FROM products WHERE item_id = '" + answers.id + "'",
+            function (err, res) {
+                if (err) throw err;
 
+                if (res[0].stock_quantity > answers.units) {
+
+                    // update databse quantity by how many units bought
+                    var newQuantity = res[0].stock_quantity - answers.units;
+                    var itemID = answers.id
+                    var unitsBought = answers.units;
+                    makeSale(newQuantity, itemID, unitsBought);
+
+                } else {
+                    console.log("Insufficient quantity!");
+                    connection.end();
+                }
+            }
+        )
     })
+}
+
+function makeSale(newQuantity, itemID, unitsBought) {
+    connection.query(
+        "UPDATE products SET stock_quantity = ? WHERE item_id = ?", [newQuantity, itemID],
+    );
+
+    connection.query(
+        "SELECT * FROM products WHERE item_id = ?", [itemID],
+        function(err, res) {
+            if (err) throw err;
+            var total = res[0].price * unitsBought;
+            console.log("\nTOTAL: $" + total);
+            console.log("Thank you for your purchase\n");
+            connection.end();
+        }
+    )
 }
--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS bamazon;
+
 CREATE DATABASE bamazon;
 
 USE bamazon;
@@ -6,17 +8,17 @@ CREATE TABLE products(
 	item_id INT AUTO_INCREMENT NOT NULL,
     product_name VARCHAR(50),
     department_name VARCHAR(100),
-    price DEC(10,4),
-    stock_quanity INT,
+    price DEC(10,2),
+    stock_quantity INT,
     PRIMARY KEY (item_id)
 );
 
-INSERT INTO products(product_name, department_name, price, stock_quanity)
+INSERT INTO products(product_name, department_name, price, stock_quantity)
 VALUES("IPhone X", "Electronics", 1000.99, 100), ("Couch", "Home", 984.22, 50), ("Roku Express", "Electronics", 26.99, 100);
 
-INSERT INTO products(product_name, department_name, price, stock_quanity)
+INSERT INTO products(product_name, department_name, price, stock_quantity)
 VALUES("Fisher-Price Learning Table", "Toys", 36.34, 50), ("Smart Home Camera", "Home", 25.98, 48), ("Mini Fridge", "Appliances", 279.99, 62);
 
-INSERT INTO products(product_name, department_name, price, stock_quanity)
+INSERT INTO products(product_name, department_name, price, stock_quantity)
 VALUES("Ice Maker", "Appliances", 117.99, 51), ("Dumbbell Pair", "Sports and Fitness", 35, 76), ("Pool Table", "Sports and Fitness", 999.99, 17),  ("Lego Batman Set", "Toys", 6.99, 40);
 
